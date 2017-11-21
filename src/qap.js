547f7816a8ce5602fa11d03cecd738ce03208fe1
Kendrick Tan <kendricktan0814@gmail.com>
2017-11-22 00:18:13 +1000

    polynomial formation

--- src/qap.js
+++ src/qap.js
@@ -6,6 +6,7 @@
 // the array is the ith degree coefficient
 // e.g. [1, 5, 4, 3, 2] = 1 + 5x + 4x^2 + 3x^3 + 2x^4
 type polynomial = number[]
+type matrix = polynomial[]
 
 // Zip array to longest
 const zipLongest = (placeholder: number = 0, ...arrays): Array<number>[] => {
@@ -67,12 +68,75 @@ const mkSingleton = (pointLoc: number, height: number, totalPts: number): polyno
     const idx = Array.from({length: totalPts}, (_, i) => i + 1)
     const fac = idx.reduce((a, b) => a * (b === pointLoc ? 1 : pointLoc - b), 1)
     
-    return idx.reduce((a, b) => b === pointLoc ? a : multiplyPolys(a, [-b, 1]), [height * 1.0 / fac])
+    return idx.reduce((a, b) => b === pointLoc ? a : multiplyPoly(a, [-b, 1]), [height * 1.0 / fac])
 }
 
-// Transpose an array of polynomial
-const transpose = (m: polynomial[]): polynomial[] => {
+// Transpose a matrix
+const transpose = (m: matrix): matrix => {
     return zipLongest(0, ...m)
 }
 
-console.log(transpose([[1,2,3,4],[5,6,7,8]]))
+// Lagrange Interpolation
+// Assumes poly[0] = p(1), poly[1] = p(2) ..., tries to find p
+// Reults expressed as [deg 0 coeff, def 1 coeff...]
+const lagrangeInterpolation = (poly: polynomial): polynomial => {
+    const p: polynomial = poly.reduce((acc, x, i) => addPoly(acc, mkSingleton(i + 1, x, poly.length)), [])
+    
+    for (let i = 0; i < poly.length; i ++) {
+        if (Math.abs(evalPoly(p, i + 1) - poly[i]) > 10**-10) {
+            throw new Error('Error calculating lagrange: ' + (p, evalPoly(p, i), i + 1))
+        }
+    }
+
+    return p
+}
+
+// Converts r1cs to qap
+// r1cs = Rank 1 constraint system
+const r1csToQap = (A: matrix, B: matrix, C: matrix): [matrix, matrix, matrix, polynomial] => {
+    const newA: matrix = transpose(A).map((x) => lagrangeInterpolation(x))
+    const newB: matrix = transpose(B).map((x) => lagrangeInterpolation(x))
+    const newC: matrix = transpose(C).map((x) => lagrangeInterpolation(x))
+
+    // Z = (x - 1) * (x - 2) ... (x - N) where N is the length of the polynomial
+    const Z: polynomial = Array.from({length: transpose(A)[0].length}, (_, i) => i + 1)
+                   .reduce((acc, x) => multiplyPoly(acc, [-x, 1]), [1])
+    
+    return [newA, newB, newC, Z]
+}
+
+// This picture might better illustrate the process: https://cdn-images-1.medium.com/max/800/1*wp6bmXoPEU_zZHzJFRq6IQ.png
+// Only thing is that its doing all the R1CS at the same time
+// A * B - C = H * Z
+const createSolutionPolynomials = (r: polynomial, newA: matrix, newB: matrix, newC: matrix): [polynomial, polynomial, polynomial, polynomial] => {
+    const Apoly: polynomial = zipLongest(0, r, newA).reduce((acc, x) => addPoly(acc, multiplyPoly([x[0]], x[1])), [])
+    const Bpoly: polynomial = zipLongest(0, r, newB).reduce((acc, x) => addPoly(acc, multiplyPoly([x[0]], x[1])), [])
+    const Cpoly: polynomial = zipLongest(0, r, newC).reduce((acc, x) => addPoly(acc, multiplyPoly([x[0]], x[1])), [])
+    const H: polynomial = subtractPoly(multiplyPoly(Apoly, Bpoly), Cpoly)
+
+    for (let i = 1; i < newA[0].length + 1; i ++ ) {
+        if (Math.abs(evalPoly(H, i) > 10**-10)) {
+            throw new Error('Error creating poly solution: ' + (evalPoly(H, i), i))
+        }
+    }
+
+    return [Apoly, Bpoly, Cpoly, H]
+}
+
+var r = [1, 3, 35, 9, 27, 31]
+var A = [[0, 1, 0, 0, 0, 0],
+[0, 0, 0, 1, 0, 0],
+[0, 1, 0, 0, 1, 0],
+[5, 0, 0, 0, 0, 1]]
+var B = [[0, 1, 0, 0, 0, 0],
+[0, 1, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0]]
+var C = [[0, 0, 0, 1, 0, 0],
+[0, 0, 0, 0, 1, 0],
+[0, 0, 0, 0, 0, 1],
+[0, 0, 1, 0, 0, 0]]
+var [Ap, Bp, Cp, Z] = r1csToQap(A, B, C)
+var [AP, BP, CP, H] = createSolutionPolynomials(r, Ap, Bp, Cp)
+console.log(divPoly(H, Z))
+// console.log(Ap, Bp, Cp)

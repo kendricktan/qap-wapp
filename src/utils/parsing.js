6eaeefe9ab630bb2b657927ab11afdb198a044b9
Kendrick Tan <kendricktan0814@gmail.com>
2017-11-25 22:27:36 +1000

    split expression into basic operations

--- src/utils/parsing.js
+++ src/utils/parsing.js
@@ -24,6 +24,10 @@ const locateBrackets = (expression: string): string[] => {
     }
   }
 
+  if (count !== 0) {
+    throw new Error('Bracket parsing error')
+  }
+
   return content
 }
 
@@ -52,11 +56,7 @@ const parseBrackets = (expression: string, symbols: Object, symI: number = 1): [
       }, expression)
 
       // Replace all content
-      bracketContent = bracketContent.map((x) => {
-        return Object.keys(symbols).reduce((acc, s) => {
-          return acc.split('(' + s + ')').join(symbols[s])
-        }, x)
-      })
+      bracketContent = locateBrackets(expression)
 
       // Replace existing content
       expr = bracketContent[i].slice(1, -1)
@@ -96,10 +96,9 @@ const parseExponents = (expression: string, symbols: Object): [string, Object] =
     // Loop through each exponents and replace with VAR
     for (let i = 0; i < exponents.length; i++) {
       let curExponent = exponents[i]
-      const varSym = 'var_' + (varI++).toString()
 
       if (acc[curExponent] === undefined) {
-        acc[curExponent] = varSym
+        acc[curExponent] = 'var_' + (varI++).toString()
       }
 
       // At the same time replace variable assignment for all existing exponent
@@ -127,18 +126,149 @@ const parseExponents = (expression: string, symbols: Object): [string, Object] =
     }
   }
 
+  // Parse the expression
+  const expExponents = expression.match(reExponent)
+
+  if (expExponents !== null) {
+    for (let i = 0; i < expExponents.length; i++) {
+      const exp = expExponents[i]
+
+      if (exponentSymbols[exp] === undefined) {
+        const sym = 'var_' + (varI++).toString()
+        expression = expression.split(exp).join(sym)
+        exponentSymbols[exp] = sym
+      }
+    }
+  }
+
   return [expression, Object.assign(exponentSymbols, symbolsCopy)]
 }
 
-// AST and Symbols to keep track of the stuff we've parsed
-let symbols = {} // symbol['x^2+1'] = 'sym_1' or smthg
-let expression = `x +((x^2-6) * (x^2+(3*x-5)-(5*x))) + x^2= 35`.replace(/\s/g, '')
-console.log(expression);
+const parseExpression = (expression: string, symbols: Object, idx: number, regex: RegExp) => {
+  // Apply on symbols
+  // Find out how many operations there are
+  // If there's > 1 then apply the parseExpression
+  const operationsRegex = /[+-/*]/g
 
-// Extract content from brackets
-[expression, symbols] = parseBrackets(expression, symbols)
-expression = expression.split('(').join('').split(')').join('');
-[expression, symbols] = parseExponents(expression, symbols)
+  symbols = JSON.parse(JSON.stringify(symbols))
+  for (let k in symbols) {
+    // How many operations are there
+    // in the symbols
+    let sym = k.match(operationsRegex)
 
-console.log(expression)
-console.log(symbols)
+    if (sym !== null) {
+      // If there's more than one operation
+      // Then we wanna reduce that to 1
+      if (sym.length > 1) {
+        let symExp = k.match(regex)
+        let newK
+
+        // If current regex doesn't find the correct operation
+        // then continue
+        if (symExp === null) {
+          continue
+        }
+
+        // Else try and replace it
+        symExp = symExp[0]
+        if (symbols[symExp] === undefined) {
+          const rep = 'rep_' + (idx++).toString()
+          newK = k.split(symExp).join(rep)
+
+          symbols[symExp] = rep
+          symbols[newK] = symbols[k]
+
+          delete symbols[k]
+        } else {
+          const rep = symbols[symExp]
+          newK = k.split(symExp).join(rep)
+
+          symbols[symExp] = rep
+          symbols[newK] = symbols[k]
+
+          delete symbols[k]
+        }
+
+        if (newK.match(operationsRegex) !== null) {
+          if (newK.match(operationsRegex.length) > 1) {
+            return parseExpression(expression, symbols, idx, regex)
+          }
+        }
+      }
+    }
+  }
+
+  // Apply on expression
+  let exp = expression.match(regex)
+  if (exp !== null) {
+    exp = exp[0]
+
+    if (symbols[exp] === undefined) {
+      const rep = 'rep_' + (idx++).toString()
+      symbols[exp] = rep
+      expression = expression.split(exp).join(rep)
+    } else {
+      const rep = symbols[exp]
+      expression = expression.split(exp).join(rep)
+    }
+
+    // If there still is another multi/div expression
+    // Apply it again
+    if (expression.match(regex) !== null) {
+      return parseRepresentation(expression, symbols, idx)
+    }
+  }
+
+  return [expression, symbols, idx]
+}
+
+const parseRepresentation = (expression: string, symbols: Object, repI: number = 1) => {
+  // Replace all the multiplication and division with expressions first
+  const mulDivExp = /[A-Za-z\d_]*[*|\/][A-Za-z\d_]*/g
+  const subAddExp = /[A-Za-z\d_]*[+|-][A-Za-z\d_]*/g;
+
+  // Parse on mutliplication and division
+  [expression, symbols, repI] = parseExpression(expression, symbols, repI, mulDivExp);
+  [expression, symbols, repI] = parseExpression(expression, symbols, repI, subAddExp)
+
+  return [expression, symbols, repI]
+}
+
+// Cleans duplicates from symbol
+const cleanSymbols = (symbols: Object): Object => {
+  const hasOperation = /[+-/*^]/g
+
+  for (var k in symbols) {
+    // If this key has no operations
+    // Remove it from symbol, and remap
+    // All symbols
+    if (k.match(hasOperation) === null) {
+      let newSymbol = Object.keys(symbols).reduce((acc, k2) => {
+        if (k2 === k) {
+          return acc
+        }
+        acc[k2.split(symbols[k]).join(k)] = symbols[k2]
+        return acc
+      }, {})
+      return cleanSymbols(newSymbol)
+    }
+  }
+
+  return symbols
+}
+
+// Symbols to keep track of the stuff we've parsed
+// let symbols = {} // symbol['x^2+1'] = 'sym_1' or smthg
+// let expression = `x**3 + 5 - (x^2)`.replace(/\s/g, '').split('**').join('^')
+// console.log(expression);
+
+// // Extract content from brackets
+// [expression, symbols] = parseBrackets(expression, symbols)
+
+// expression = expression.split('(').join('').split(')').join('');
+// [expression, symbols] = parseExponents(expression, symbols);
+// [expression, symbols, _] = parseRepresentation(expression, symbols)
+
+// symbols = cleanSymbols(symbols)
+
+// console.log(symbols)
